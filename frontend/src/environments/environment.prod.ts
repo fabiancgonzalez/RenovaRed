fcec746bd90b7e515ff7e8014d5c3a23633288ee
fabiancgonzalez <ceferinomonier@gmail.com>
2026-04-02 22:00:46 -0300

    chatbot apiurl

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://renovared-1shf.onrender.com/api',
-  chatApiUrl: '/api/chat',
+  chatApiUrl: 'https://renovared-1shf.onrender.com/api/chat',
 
   googleClientId: ''
 };
