27405885963a46f52018a556316c1ae75082d8ce
fabiancgonzalez <ceferinomonier@gmail.com>
2026-04-02 21:48:25 -0300

    fix Vista PDF

--- frontend/src/app/pages/help/help.component.ts
+++ frontend/src/app/pages/help/help.component.ts
@@ -1,17 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-help',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css']
 })
 export class HelpComponent {
-  readonly pdfHref = '/assets/help/Ayuda%20RenovaRed.pdf#view=FitH';
+  readonly pdfHref = '/assets/help/Ayuda%20RenovaRed.pdf';
   readonly pdfUrl: SafeResourceUrl;
+  readonly isMobile: boolean;
 
   constructor(private readonly sanitizer: DomSanitizer) {
-    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfHref);
+    this.pdfUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfHref + '#view=FitH');
+    this.isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(
+      typeof navigator !== 'undefined' ? navigator.userAgent : ''
+    );
   }
 }
